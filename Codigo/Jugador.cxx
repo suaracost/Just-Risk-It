28d5a481ee746ee7c7d609e8e1a589e85903eef3
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2023-08-27 21:55:22 -0500

    Add files via upload

---
+++ Codigo/Jugador.cxx
@@ -0,0 +1,10 @@
+#include "Jugador.h"
+
+#include <iostream>
+
+Jugador::Jugador(std::string nombreJug, std::string colorJug, int cantiTrop)
+{
+  nombreJugador = nombreJug;
+  colorJugador = colorJug;
+  cantiTropas = cantiTrop;
+}
