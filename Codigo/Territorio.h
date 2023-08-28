77a712468a44ba7fed7271cc6578b3f15d895e0c
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2023-08-27 21:53:30 -0500

    Add files via upload

---
+++ Codigo/Territorio.h
@@ -0,0 +1,19 @@
+#ifndef __TERRITORIO__H__
+#define __TERRITORIO__H__
+
+#include <string> // librerías que usaremos
+
+class Territorio
+{
+  public: //Atributos
+    std::string nombreTerritorio;
+    //std::string vecinos[];
+    std::string duenio;
+    int numTropas;
+    bool tomado;
+
+  public: //Metodos
+    Territorio(std::string nombreTer); 
+};
+
+#endif
