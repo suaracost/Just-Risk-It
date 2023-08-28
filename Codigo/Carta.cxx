a5e42fdfe907a8d5d50ec27937723a4524c551ee
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2023-08-27 21:54:07 -0500

    Add files via upload

---
+++ Codigo/Carta.cxx
@@ -0,0 +1,10 @@
+#include "Carta.h"
+
+#include <iostream>
+
+Carta::Carta(std::string nombre, std::string tipo, std::string descri)
+{
+  nombreCarta = nombre;
+  tipoCarta = tipo;
+  descripcion = descri;
+}
