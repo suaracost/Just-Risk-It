1f8f79f0f8e75f70debe1e8cf26a519cb1951370
Alejandro Suarez <alejandrosuarezacosta@gmail.com>
2023-08-31 10:37:49 -0500

    Update Partida.cxx

--- Codigo/Partida.cxx
+++ Codigo/Partida.cxx
@@ -641,6 +641,10 @@ void Partida::atacar()
 
 void Partida::fortificar()
 {
+  //Pruebas en github desktop
+  int una = 1;
+  std::cout<<una;
+
   std::string jug = turnos.front();
 
   bool puede = false, puede2 = false;
