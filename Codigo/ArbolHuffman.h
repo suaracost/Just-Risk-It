1a53ab00d95c3c7d92ad31d8bc2bb340d5c89bfe
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2023-10-08 20:00:55 -0500

    Add files via upload

---
+++ Codigo/ArbolHuffman.h
@@ -0,0 +1,26 @@
+#ifndef ARBOLHUFFMAN_H_
+#define ARBOLHUFFMAN_H_
+
+#include "NodoHuffman.h"
+
+class HuffmanArbol
+{
+private:
+    HuffmanNodo *raiz;
+    std::vector <std::pair <char,std::string>> codigos;
+
+public:
+    HuffmanArbol();
+    void generarArbol(char dato[], long freq[],int size);
+    struct comparar;
+    void guardarCodigos(HuffmanNodo *inicio, std::string str);
+    void imprimirCodigos();
+    std::string obtenerCodigo(char caracter);
+    std::string cifrar(std::string secunecia);
+    std::string desCifrar(std::string cif, long longiSec);
+    HuffmanNodo * deCodificar(HuffmanNodo *nodo, char dir, int &cont, std::string &des);
+    HuffmanNodo * getRaiz();
+};
+
+#include "ArbolHuffman.cxx"
+#endif 
---
+++ Codigo/NodoHuffman.h
@@ -0,0 +1,25 @@
+#ifndef NODOHUFFMAN_H_
+#define NODOHUFFMAN_H_
+
+class HuffmanNodo
+{
+private:
+    char dato;
+    unsigned int freq;
+    HuffmanNodo *der, *izq;
+    
+public:
+    HuffmanNodo();
+    HuffmanNodo(char dato, unsigned int freq);
+    HuffmanNodo* obtenerHijoIzq();
+    HuffmanNodo* obtenerHijoDer();
+    void fijarHijoDer(HuffmanNodo* der);
+    void fijarHijoIzq(HuffmanNodo* izq);
+    unsigned int obtenerFreq();
+    char obtenerDato();
+    bool esHoja();
+};
+
+#include "NodoHuffman.cxx"
+
+#endif
